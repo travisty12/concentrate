570d9a223ae2602780c28e43c866d40ff2656d9b
Gary Bermudez and Travis Toal <gary+travis@>
2019-03-11 17:00:36 -0700

    fix image bug by importing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import './sass/styles.scss';
 import $ from 'jquery';
+import jack from './assets/img/jack.png';
+import queen from './assets/img/queen.png';
+import king from './assets/img/king.png';
+import ace from './assets/img/ace.png';
+import joker from './assets/img/joker.png';
+
 
 function prep() {
   var html = "";
@@ -19,13 +25,13 @@ $(document).ready(function() {
   var html = prep();
   var hasClicked = false;
   $(".wrapper").append(html);
-  var cardArr = ["jack","queen","king","ace","joker"];
+  var cardArr = [jack,queen,king,ace,joker];
   // var current;
   $(".card").click(function() {
     // var lastCard = current;
     // current = $(this);
     var card = cardArr[Math.floor(5*Math.random(1))];
-    var cardHtml = "<img src='assets/img/" + card + ".png' alt='card'>";
+    var cardHtml = "<img src=" + card + " alt='card'>";
     if (!hasClicked) {
       $(this).addClass("flip");
       $(this).removeClass("flip");
